9e4fc7015fa289ae3e8738c5020899a40d14fd00
Spring157YMH <5120100660@bipt.edu.cn>
2015-05-09 16:59:11 +0800

    Update triangle.js

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -5,6 +5,17 @@ var triangle = {
     		return true ;
     },
     isIsosceles : function(a,b,c) {
+    	if (a!=b||b!=c||a!=c) return false;
+    	if(a<=0) return false;
+    	if(b<=0) return false;
+    	if(c<=0) return false;
     	    return true ;
+    },
+    isIsosceles : function(a,b,c) {
+    	if ((a+b<=c||a+c<=b||b+c<=a)$$(a-b>=c||a-c>=b||b-c>=a)) return false;
+    	if(a<=0) return false;
+    	if(b<=0) return false;
+    	if(c<=0) return false;
+    	    return true ;  
     }
 };
